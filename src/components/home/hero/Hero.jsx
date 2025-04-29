8a634cc70a78748f6faa59fb04c61c76b3d49e04
unknown <ammariqbal983@gmail.com>
2025-04-29 23:31:43 +0700

    menambahkan bg pada home dan menyesuaikan figma

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -8,14 +8,14 @@ const Hero = () => {
       <section className='hero'>
         <div className='container'>
           <div className='row'>
-            <Heading subtitle='WELCOME TO ACADEMIA' title='Best Online Education Expertise' />
-            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <Heading subtitle='WELCOME TO ASA ACADEMY' title='Materi pilihan terbaik dari yang terbaik' />
+            <p>Disini menyediakan materi-materi tentang pemrograman dari yang dasar sampai menjadi profesional</p>
             <div className='button'>
               <button className='primary-btn'>
-                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+                MULAI SEKARANG! <i className='fa fa-long-arrow-alt-right'></i>
               </button>
               <button>
-                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+                LIHAT MATERI <i className='fa fa-long-arrow-alt-right'></i>
               </button>
             </div>
           </div>
