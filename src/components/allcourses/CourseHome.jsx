b659b084c88617b1633c6f874200882fc51edf04
saavaurs <savaaurellia945@gmail.com>
2025-05-06 16:23:39 +0700

    berubahan tampilan courses

--- src/components/allcourses/CourseHome.jsx
+++ src/components/allcourses/CourseHome.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Back from "../common/back/Back";
-import CoursesCard from "./CoursesCard";
 import OnlineCourses from "./OnlineCourses"; // Tambahkan impor ini
 import "./courses.css";
 
 const CourseHome = () => {
   return (
     <div className="coursesContainer">
-      <Back title="Explore Courses" />
-      <CoursesCard />
       <OnlineCourses />
     </div>
   );
